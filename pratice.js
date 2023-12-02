c44371230f157f83efaa539b63a9bafd9c50893f
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2023-12-02 22:42:21 +0545

    Add files via upload

---
+++ pratice.js
@@ -0,0 +1,119 @@
+// this is for map tag //
+// const myMap = new Map([
+//     ["name", "John"],
+//     ["age", 30],
+//     ["isStudent", false]
+//   ]);
+  
+//    console.log(myMap.get("name")); // Output: John
+//   console.log(myMap.has("age"));  // Output: true
+  
+// myMap.forEach((value, key) => console.log(key, value));
+  
+//   myMap.delete("age");
+// console.log(myMap.has("age")); // Output: false
+  
+// console.log(myMap.size); // Output: 2
+//    myMap.clear();
+//  console.log(myMap.size); // Output: 0
+  
+// const myMap = new Map([
+//     ["name", "abhishek"],
+//     ["age", "20"],
+//     ["married", "false"]
+// ])
+// myMap.forEach((value,key) =>console.log(key,value));
+
+
+//filter//
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const evenNumbers = numbers.filter(function (number) {
+//   return number % 2 === 0;
+// });
+
+// console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+
+// console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
+
+//reduce
+
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const sum = numbers.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// }, 0);
+
+// console.log(sum); // Output: 15
+
+// this means that add to the any number by usnig the SubmitEvent.reduce.
+
+
+
+
+// variable(var:)
+// a variable is a named container that can hold various types of data, such as numbers, strings, objects, and functions. 
+// var greeting = "Hello, World!";
+// Get the HTML element by its ID
+// var greetingElement = document.getElementById("greeting");
+
+// // Update the HTML content using the variable
+// greetingElement.innerHTML = greeting;
+
+
+// var contentElement= document.getElementById("content");
+// contentElement.innerHTML = content;
+
+
+
+
+// string (how to use)
+// In JavaScript, a string is a data type used to represent text. 
+
+// let singleQuoted = 'This is a single-quoted string';
+// let doubleQuoted = "This is a double-quoted string";
+// // let backticks = `This is a backtick-quoted string`;
+// let firstName = 'John';
+// let lastName = 'Doe';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName); // Output: "John Doe"
+
+// let firstName = 'ABhishek';
+// let lastName = 'Sardar';
+// let fullName = firstName + '   '+ lastName;
+// console.log(fullName);
+
+// let firstName = "Bijay";
+// let lastName = "sardar";
+// let fullName = firstName + ' ' +lastName;
+// console.log(fullName);
+
+// let text = "Hello, World!";
+// let length = text.length;
+// console.log(length); // Output: 13
+
+// let text = "Abhishek, sardar";
+// let length = text.length;
+// console.log(length);
+
+// let text = "Hello";
+// let firstCharacter = text[0]; // 'H'
+// let secondCharacter = text[1]; // 'e'
+
+// let sentence = "This is a sample sentence.";
+// let upperCaseSentence = sentence.toUpperCase();
+// console.log(upperCaseSentence); // Output: "THIS IS A SAMPLE SENTENCE"
+
+// let position = sentence.indexOf("sample");
+// console.log(position); // Output: 10 (position of "sample" in the string)
+
+let myVar = 42; // myVar is now of type Number
+myVar = "Hello"; // myVar is now of type String
+console.log(myVar);
+
